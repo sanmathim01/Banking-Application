0bcc3ef20dfbb32243db8da1b184029af8f51c9e
sanmathim01 <sanmathim01@outlook.com>
2024-11-20 16:52:08 +0000

    Backend

---
+++ banking-backend/src/test/java/com/example/banking_backend/BankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
